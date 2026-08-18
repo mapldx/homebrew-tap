13cb4bf482988b4b2ff00143444a7dd7af266214
Zachary Will Sy <git@fwd.zacharymail.com>
2026-08-17 22:00:00 -0500

    fix juno server package

--- Casks/juno.rb
+++ Casks/juno.rb
@@ -1,6 +1,6 @@
 cask "juno" do
   version "0.1.0"
-  sha256 "0b02b77895d971e0821c539e37e698a0dd61b2c1df5e344bb4f19e9812e6016d"
+  sha256 "5ee9e089a01c2fa7450fc431bfadfd122677a969da0fa3600560f935d85101de"
 
   url "https://github.com/mapldx/homebrew-tap/releases/download/juno-v#{version}/Juno-#{version}-macos-arm64.zip",
       verified: "github.com/mapldx/homebrew-tap/"
